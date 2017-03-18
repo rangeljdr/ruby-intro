ffe7f548af49764443b9b1092e220271bd22563e
Juan David <rangeljdr@gmail.com>
2017-03-17 19:12:36 -0500

    Update 3/17

--- challenges/active-record/5-validations/source/app/models/rating.rb
+++ challenges/active-record/5-validations/source/app/models/rating.rb
@@ -1,2 +1,11 @@
 class Rating < ActiveRecord::Base
+
+validates :coolness, :cuteness, :judge_id, :dog_id, { :presence => true }
+
+validates :coolness, :numericality => {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
+
+validates :cuteness, :numericality => {greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
+
+validates :judge_id, :dog_id, { :uniqueness => true }
+
 end
